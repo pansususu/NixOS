7d90029de69f908dc200598f3da7e2b8858bb261
pansususu <juaniiidlrz@gmail.com>
2026-05-21 19:04:10 -0300

    NixOS 1 pull

---
+++ configuration.nix
@@ -0,0 +1,114 @@
+{ config, lib, pkgs, inputs, ... }:
+
+{
+  imports = [ 
+    ./hardware-configuration.nix
+  ];
+
+  # Bootloader & System
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  networking.hostName = "nixos";
+  networking.networkmanager.enable = true;
+  time.timeZone = "America/Asuncion";
+
+  # Language & Console
+  i18n.defaultLocale = "es_PY.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "la-latin1";
+  };
+
+  # Sound 
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
+  services.libinput.enable = true;
+
+  # User
+  users.users.sabrina = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "audio" "video" ];
+    packages = with pkgs; [ tree ];
+  };
+
+  # Software Unfree & Flatpak
+  nixpkgs.config.allowUnfree = true;
+  services.flatpak.enable = true;
+
+  # Spicetiy
+   programs.spicetify = let
+    spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
+  in {
+    enable = true;
+    theme = spicePkgs.themes.catppuccin;
+    colorScheme = "mocha";
+    enabledExtensions = with spicePkgs.extensions; [
+      adblock
+      hidePodcasts
+      shuffle
+    ];
+  };
+
+  # Packages
+  environment.systemPackages = with pkgs; [
+    nano
+    wget
+    alacritty
+    firefox
+    git
+    discord
+  ];
+
+  # Enviroment Variables
+   environment.sessionVariables = {
+  XDG_DATA_DIRS = [
+    "/var/lib/flatpak/exports/share"
+    "$HOME/.local/share/flatpak/exports/share"
+    ];
+  };
+
+  # Programs (Steam, GPG, MTR)
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    gamescopeSession.enable = true;
+  };
+
+  # Nvidia & Graphics
+  services.xserver.videoDrivers = [ "nvidia" ];
+  
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    open = false; # Owner Driver
+    powerManagement.enable = false; # Change to true if you have problems to poweroff/sleep
+  };
+
+  hardware.xpadneo.enable = true;
+  hardware.enableRedistributableFirmware = true;
+
+  # DE (Kde Plasma 6)
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  # SSH
+  services.openssh.enable = true;
+  
+  # Flake 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  system.stateVersion = "25.11"; 
+}

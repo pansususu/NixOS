ab620be367c4a1f5840938cb98e0081bceea6fee
sabrina <sabrina@finix>
2026-07-23 11:14:14 -0300

    auto: update config

--- modules/users.nix
+++ modules/users.nix
@@ -3,7 +3,6 @@
 {
   users.users.sabrina = {
     isNormalUser = true;
-    initialPassword = "sabrina";
     extraGroups = [ "wheel" "audio" "video" "render" "input" "fuse" "libvirtd" "seat" ];
     packages = with pkgs; [ tree ];
   };
